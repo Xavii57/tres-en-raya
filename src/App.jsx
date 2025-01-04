a219e72d08de61665ff0b6ce1757573026f7c702
Xavii57 <andrade.javier7250@gmail.com>
2025-01-04 16:33:12 -0300

    Primer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <h1>Tres en linea</h1>
 }
+
+export default App
