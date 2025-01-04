1813474f7e0c9343871379708f52d408ff11c6e1
Xavii57 <andrade.javier7250@gmail.com>
2025-01-04 16:57:51 -0300

    Agregar tablero y numeros

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css'
+import "./App.css";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const board = Array(9).fill(null);
+
+const Square = ({ children, uptdateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
 
 function App() {
-  return <h1>Tres en linea</h1>
+  return (
+    <main className="board">
+      <h1>Tres en linea</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {index}
+            </Square>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
