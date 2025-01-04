709990eb9b7346bcc67644bf247807fd6668d7d5
Xavii57 <andrade.javier7250@gmail.com>
2025-01-04 17:51:04 -0300

    cambios mayores

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const TURNS = {
@@ -5,24 +6,53 @@ const TURNS = {
   O: "o",
 };
 
-const board = Array(9).fill(null);
+const Square = ({ children, isSelected, uptdateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
 
-const Square = ({ children, uptdateBoard, index }) => {
-  return <div className="square">{children}</div>;
+  const handleClick = () => {
+    uptdateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const uptdateBoard = (index) => {
+    // no actualizamos esta posición
+    // si ya tiene algo
+    if (board[index]) return
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambio de turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  };
+
   return (
     <main className="board">
       <h1>Tres en linea</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Square key={index} index={index}>
-              {index}
+            <Square key={index} index={index} uptdateBoard={uptdateBoard}>
+              {board[index]}
             </Square>
           );
         })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
