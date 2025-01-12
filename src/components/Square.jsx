652dafc14f9314b462663a03a4ea6ae527e50ef3
Xavii57 <andrade.javier7250@gmail.com>
2025-01-12 00:30:49 -0300

    Arreglasr error de esLint

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,13 @@
 export const Square = ({ children, isSelected, uptdateBoard, index }) => {
-    const className = `square ${isSelected ? "is-selected" : ""}`;
-  
-    const handleClick = () => {
-      uptdateBoard(index);
-    };
-  
-    return (
-      <div onClick={handleClick} className={className}>
-        {children}
-      </div>
-    );
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    uptdateBoard(index);
   };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
