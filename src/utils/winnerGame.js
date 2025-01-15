bf072b925183ba864ea68f9dab36d29625bdd140
Xavii57 <andrade.javier7250@gmail.com>
2025-01-15 15:03:14 -0300

    Formatear

--- src/utils/winnerGame.js
+++ src/utils/winnerGame.js
@@ -1,24 +1,24 @@
-import { WINNER_COMBOS } from "../constants.js";
+import { WINNER_COMBOS } from '../constants.js'
 
 export const checkWinner = (boardToCheck) => {
   // revisar todas la combinaciones ganadoras
   for (const combo of WINNER_COMBOS) {
-    const [a, b, c] = combo;
+    const [a, b, c] = combo
     if (
       boardToCheck[a] && // o => x u o
       boardToCheck[a] === boardToCheck[b] &&
       boardToCheck[a] === boardToCheck[c]
     ) {
-      return boardToCheck[a]; // x u o
+      return boardToCheck[a] // x u o
     }
   }
   // si no hay ganador
-  return null;
-};
+  return null
+}
 
 export const checkEndGame = (newBoard) => {
   // revisamos si hay un empate
   // si no hay más espacios vacíos
   // en el tablero
-  return newBoard.every((square) => square !== null);
-};
+  return newBoard.every((square) => square !== null)
+}
